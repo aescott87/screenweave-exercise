aa995c16ac08990c377f4ef45f92c5c678201c0f
Ashley Scott <aescott87@gmail.com>
2020-09-05 15:12:09 -0500

    Videos display on the page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Video Player:</h1>
+      <div className="videos">
+        <h2>Video 1:</h2>
+        <video width="400" height="320" controls>
+        <source src="http://reflect-tightytv-vod.cablecast.tv/vod/2-TRMS-Medium-v1.mp4" type="video/mp4" />
+        </video>
+        <h2>Video 2:</h2>
+        <video width="400" height="320" controls>
+        <source src="http://reflect-tightytv-vod.cablecast.tv/vod/52-CTV-Needs-Interns-Promo-High-v1.mp4" type="video/mp4" />
+        </video>
+        <h2>Video 3:</h2>
+        <video width="400" height="320" controls>
+        <source src="http://reflect-tightytv-vod.cablecast.tv/vod/3-NAB-2014-Artbeats-30min-High-v4.mp4" type="video/mp4" />
+        </video>
+      </div>
     </div>
   );
 }

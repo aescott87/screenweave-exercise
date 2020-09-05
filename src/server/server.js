e1b4195d5bfe028e0607b0bdccd36a3b436316d6
Ashley Scott <aescott87@gmail.com>
2020-09-05 16:29:19 -0500

    Set up basic server setup

---
+++ src/server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const videoPlayer = require('./routes/videoPlayer.router');
+const PORT = process.env.PORT || 5000;
+
+/** ---------- MIDDLEWARE ---------- **/
+app.use(bodyParser.json()); // needed for post/put requests
+app.use(express.static('build'));
+
+/** ---------- EXPRESS ROUTES ---------- **/
+app.use('/video-player', videoPlayer)
+
+/** ---------- START SERVER ---------- **/
+app.listen(PORT,  () => {
+    console.log('Listening on port: ', PORT);
+});

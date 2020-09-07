8c7204e0c35041034dae00bfc3caacf0d9d1134a
Ashley Scott <aescott87@gmail.com>
2020-09-07 13:04:12 -0500

    Fixed server start up issue and set up socket on server side

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const videoPlayer = require('./routes/videoPlayer.router');
 const PORT = process.env.PORT || 5000;
+const io = require('socket.io')();
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for post/put requests
@@ -12,6 +13,19 @@ app.use(express.static('build'));
 app.use('/video-player', videoPlayer)
 
 /** ---------- START SERVER ---------- **/
-app.listen(PORT,  () => {
+/*app.listen(PORT,  () => {
     console.log('Listening on port: ', PORT);
+});*/
+
+/** ---------- SOCKET SETUP ---------- **/
+io.listen(PORT);
+console.log('Listening on port ', PORT);
+
+io.on('connection', (client) => {
+    client.on('subscribeToTimer', (interval) => {
+        console.log('client is subscribing to timer with interval ', interval);
+        setInterval(() => {
+            client.emit('timer', new Date());
+        }, interval);
+    });
 });
--- src/App.js
+++ src/App.js
@@ -12,10 +12,6 @@ function App() {
         </video>
         <h2>Video 2:</h2>
         <video width="400" height="320" controls>
-        <source src="http://reflect-tightytv-vod.cablecast.tv/vod/52-CTV-Needs-Interns-Promo-High-v1.mp4" type="video/mp4" />
-        </video>
-        <h2>Video 3:</h2>
-        <video width="400" height="320" controls>
         <source src="http://reflect-tightytv-vod.cablecast.tv/vod/3-NAB-2014-Artbeats-30min-High-v4.mp4" type="video/mp4" />
         </video>
       </div>

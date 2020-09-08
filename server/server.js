78aae6e1a5b4a6029efffbe93a18bfe8b5ebb667
Ashley Scott <aescott87@gmail.com>
2020-09-07 19:19:01 -0500

    Set up socket connections on server and client for total viewer count

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const videoPlayer = require('./routes/videoPlayer.router');
 const PORT = process.env.PORT || 5000;
 const io = require('socket.io')();
+let totalCount = 0;
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for post/put requests
@@ -22,10 +23,13 @@ io.listen(PORT);
 console.log('Listening on port ', PORT);
 
 io.on('connection', (client) => {
-    client.on('subscribeToTimer', (interval) => {
-        console.log('client is subscribing to timer with interval ', interval);
-        setInterval(() => {
-            client.emit('timer', new Date());
-        }, interval);
+    /*client.on('setTotalViewerCount', (integer) => {
+        console.log('client is increasing viewer count by ', integer);
+    });*/
+    client.emit('totalViewerCount', totalCount);
+    client.on('play', (id) => {
+        totalCount += 1
+        io.sockets.emit('totalViewerCount', totalCount);
     });
 });
+
--- src/App.js
+++ src/App.js
@@ -3,38 +3,38 @@ import openSocket from 'socket.io-client';
 import './App.css';
 const socket = openSocket('http://localhost:5000');
 
-
 class App extends Component {
 
-  subscribeToTimer = (callback) => {
-    socket.on('timer', timestamp => callback(null, timestamp));
-    socket.emit('subscribeToTimer', 1000);
-  }
-
-  constructor(props) {
-    super(props);
-    
-    this.subscribeToTimer((err, timestamp) => this.setState({
-      timestamp
-    }));
-  }
-
   state = {
-    timestamp: 'No timestamp yet'
+    totalViewerCount: 0
   };
+
+  componentDidMount() {
+    socket.on('connect', () => {
+      console.log('Client is connected.')
+    });
+    socket.on('totalViewerCount', (count) => {
+      console.log(count);
+      this.setState({
+        totalViewerCount: count
+      })
+    })
+  }
+
+  
 
   render() {
     return (
       <div className="App">
-        <p>This is the timer value: {this.state.timestamp}</p>
+        <p>Total Current Video Viewers: {this.state.totalViewerCount}</p>
         <h1>Video Player:</h1>
         <div className="videos">
           <h2>Video 1:</h2>
-          <video width="400" height="320" controls>
+          <video id="vid1" width="400" height="320" controls onPlay={() => socket.emit('play', 1)}>
             <source src="http://reflect-tightytv-vod.cablecast.tv/vod/2-TRMS-Medium-v1.mp4" type="video/mp4" />
           </video>
           <h2>Video 2:</h2>
-          <video width="400" height="320" controls>
+          <video id="vid2" width="400" height="320" controls onPlay={() => socket.emit('play', 2)}>
             <source src="http://reflect-tightytv-vod.cablecast.tv/vod/3-NAB-2014-Artbeats-30min-High-v4.mp4" type="video/mp4" />
           </video>
         </div>
